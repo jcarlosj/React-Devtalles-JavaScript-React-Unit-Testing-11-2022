3d92413a0c18cbe0745dfaa33f6e63c3bf7e3cdb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-03 17:04:49 -0500

    [+] JS: Import, Export / Default & Destructuring Import

--- src/data/heroes.js
+++ src/data/heroes.js
@@ -1,7 +1,11 @@
-export const heroes = [
+const heroes = [
     { id: 1, name: 'Batman', owner: 'DComics' },
     { id: 2, name: 'Spiderman', owner: 'Marvel' },
     { id: 3, name: 'Superman', owner: 'DComics' },
     { id: 4, name: 'Flash', owner: 'DComics' },
     { id: 5, name: 'Wolverine', owner: 'Marvel' },
 ];
+
+export const owners = [ 'DComics', 'Marvel' ];
+
+export default heroes;
--- src/functions/heroes.js
+++ src/functions/heroes.js
@@ -1,7 +1,7 @@
-import { heroes } from '../data/heroes';
+import dataHeroes, { owners } from '../data/heroes.js';
 
-console.log( heroes );
+console.log( dataHeroes, owners );
 
-export const getHeroById = ( id ) => heroes.find( hero => hero.id === id );
+export const getHeroById = ( id ) => dataHeroes.find( hero => hero.id === id );
 
-export const  getHeroesByOwner = ( owner ) => heroes.filter( hero => hero.owner === owner );
+export const  getHeroesByOwner = ( owner ) => dataHeroes.filter( hero => hero.owner === owner );

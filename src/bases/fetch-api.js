5de2e5cea0b4bf91ef6ace0f5e604f290cefaf7e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-03 18:22:35 -0500

    [+] JS: Fetch API / Promesas encadenadas

--- src/bases/fetch-api.js
+++ src/bases/fetch-api.js
@@ -8,18 +8,16 @@ const
 
 const request = fetch( url );
 
-request.then( response => {
-    // console.log( response );
-    response.json().then( data => {
-        const { url } = data.data.images.original;
+request
+    .then( response => response.json() )
+    .then( ({ data }) => {
+        const { url } = data.images.original;
         console.log( url );
 
         const img = document.createElement( 'img' );
 
         img.src = url;
         document.body.append( img );
-    });
-})
-.catch( console.warn );
-
+    })
+    .catch( console.warn );
 

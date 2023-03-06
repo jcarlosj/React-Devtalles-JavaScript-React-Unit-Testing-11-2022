9db70644a4166a5fd529b03d4455038bcfc539fb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-05 20:19:02 -0500

    [+] Component - App: Comunicacion entre componentes - Props

--- src/components/HelloWorldApp.jsx
+++ src/components/HelloWorldApp.jsx
@@ -1,10 +1,8 @@
 // Definir valores fuera del componente permite:
 // - Que el valor no sea re-procesado o re-renderizado por el componente.
-// - Si los valores no tienen ninguna dependencia del componente podemos ponerla fuera de nuestros funcional components
-const name = 'Juan' ;   
+// - Si los valores no tienen ninguna dependencia del componente podemos ponerla fuera de nuestros funcional components 
 const numbers = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9, 10];
 const person = { name: 'Juan Carlos', age: 45 };
-const message = 'This is my first app.';
 
 // Arrow Function
 const addNumbers = () => {
@@ -16,12 +14,12 @@ const addNumbers = () => {
 }
 
 // Functional Component
-function App() {
+function App( props) {
     return (
         <>
             {/* Las diferentes formas de imprimir valores de constantes, variables, objetos o funciones en el JSX (cuerpo) del componente */}
-            <h1>Hello, { name }!</h1>
-            <p>{ message }</p>
+            <h1>Hello, { props.name }!</h1>
+            <p>{ props.message }</p>
             <p><b>Operations: </b> { 4 + 5 }</p>
             <p><b>Array: </b> { numbers }</p>
             <p><b>Object: </b> <code>{ JSON.stringify( person ) }</code></p>       
--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import ReactDOM from 'react-dom/client';
 import App from './components/HelloWorldApp';
 import '../src/styles.css';
 
+const message = 'This is my first app.';
+
 
 ReactDOM.createRoot(
     document.getElementById( 'root' )
 ).render(
     <React.StrictMode>
-        <App />
+        <App 
+            name = 'Juan Carlos'
+            message={ message } 
+        />
     </React.StrictMode>
 );

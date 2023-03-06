dfb058e0fe7c3e0988bf1d2fd700fdf89b305c90
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-05 20:08:29 -0500

    [+] Component - App: Imprimir valores dentro del cuerpo del componente

--- src/components/HelloWorldApp.jsx
+++ src/components/HelloWorldApp.jsx
@@ -1,9 +1,31 @@
+// Definir valores fuera del componente permite:
+// - Que el valor no sea re-procesado o re-renderizado por el componente.
+// - Si los valores no tienen ninguna dependencia del componente podemos ponerla fuera de nuestros funcional components
+const name = 'Juan' ;   
+const numbers = [1, 2, 3, 4, 5, 6 ,7 ,8 ,9, 10];
+const person = { name: 'Juan Carlos', age: 45 };
+const message = 'This is my first app.';
+
+// Arrow Function
+const addNumbers = () => {
+    const initialValue = 0;
+
+    return numbers.reduce( ( acummulator, currentValue ) => {
+        return acummulator + currentValue;
+    }, initialValue );
+}
+
 // Functional Component
 function App() {
     return (
         <>
-            <h1>Hello, World!</h1>
-            <p>This is my first app.</p>
+            {/* Las diferentes formas de imprimir valores de constantes, variables, objetos o funciones en el JSX (cuerpo) del componente */}
+            <h1>Hello, { name }!</h1>
+            <p>{ message }</p>
+            <p><b>Operations: </b> { 4 + 5 }</p>
+            <p><b>Array: </b> { numbers }</p>
+            <p><b>Object: </b> <code>{ JSON.stringify( person ) }</code></p>       
+            <p><b>Function: </b>{ addNumbers() }</p>
         </>
     )
 }

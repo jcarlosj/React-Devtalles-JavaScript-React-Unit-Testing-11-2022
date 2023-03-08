90e1121a379cdefb0cb998534f67d4c85dd04205
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-07 22:42:02 -0500

    [+] Component - Counter: Define su estado y toma el valor por defecto pasado por props

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 
 const CounterApp = ({ value }) => {
-    
+    const [ counter, setCounter ] = useState( value );
+
     const handleAdd = ( event ) => {
-        console.log( event );
+        // setCounter( counter + 1 );               // Forma 1
+        setCounter( newValue => newValue + 1 );     // Forma 2
     }
 
     return (
         <>
             <h1>Counter App</h1>   
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button
                 onClick={ handleAdd }
             >

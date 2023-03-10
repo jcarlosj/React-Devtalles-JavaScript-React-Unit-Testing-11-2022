471bab99f96408d4d1b77df5ee49fc754d894aeb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-09 19:54:40 -0500

    [+] ./src/test: Define test 'destructuring-objects' [$] ./src: Refactoriza funcionalidad de acuerdo al test definido

--- src/bases/destructuring-objects.js
+++ src/bases/destructuring-objects.js
@@ -1,42 +1,16 @@
 // Desestructuración o Asignación desestructurante de objetos
 // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 
-const person = {
-    firstName: 'Juan',
-    age: 45,
-    nickname: 'jcarlosj',
-    coords: {
-        lat: 4.7392514, 
-        lng: -74.0403743
-    }
-}
-
-console.log( person.firstName, person.age, person.nickname );
-
-const { firstName, age, nickname } = person;    // Desestructuración
-console.log( firstName, age, nickname );
-
-
-const { firstName: nombre, age: edad, nickname: apodo } = person;    // Desestructuración
-console.log( nombre, edad, apodo );
-
-
 // Desestructuración en el argumento de una función
-const getUser = ({ firstName, age, profession = 'Developer' }) => {
-    console.log( `Hola ${ firstName } ¡Wow! ¿Tienes ${ age } años y eres ${ profession }?` );
+export const getUserMessage = ({ firstName, age, profession = 'Developer' }) => {
+    return `Hola ${ firstName } ¡Wow! ¿Tienes ${ age } años y eres ${ profession }?`;
 }
 
-getUser( person );
 
-
-const useContext = ({ nickname, coords, profession = 'Developer' }) => {
+export const useContext = ({ nickname, coords, profession = 'Developer' }) => {
     return {
         sobrenombre: nickname,
         coords,
         profession
     }
 }
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const { sobrenombre, profession, coords: { lat, lng } } = useContext( person );     // Nested destructuring of objects
-console.log( sobrenombre, profession, lat, lng );
---
+++ src/test/bases/destructuring-objects.test.js
@@ -0,0 +1,34 @@
+import { getUserMessage, useContext } from "../../bases/destructuring-objects";
+
+describe( 'Test: destructuring-objects', () => {
+    test( 'getUserMessage debe retornar un mensaje', () => {
+        const 
+            testUser = { 
+                firstName: 'John', 
+                age: 27, 
+                profession: 'Doctor' 
+            },
+            message = getUserMessage( testUser ),
+            expectedMessage = `Hola ${ testUser.firstName } ¡Wow! ¿Tienes ${ testUser.age } años y eres ${ testUser.profession }?`;
+
+        expect( message ).toBe( expectedMessage );
+    });
+
+    test( 'useContext debe retornar un objeto con parametro "profession" predeterminado', () => {
+        const testUser = { 
+            nickname: 'jhonny',
+            coords: { lat: -1, lng: -4 }
+        };
+
+        const user = useContext( testUser );
+
+        console.log( user );
+
+        expect( user ).toStrictEqual({ 
+            sobrenombre: 'jhonny',
+            coords: { lat: -1, lng: -4 },
+            profession: 'Developer'
+        });
+        
+    });
+});

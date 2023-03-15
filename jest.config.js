a745b425ca092d88f5549179bafabfa52bd0258e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-03-15 14:19:44 -0500

    [+] jest.config.js: Configuracion de Jest [+] jest.setup.js: Integracion de Jest con la biblioteca de pruebas de React

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
     // testMatch: [ '<rootDir>/tests/**/?(*.)+(spec|test).js' ]
-    
     // Configuracion para admitir el uso de Fetch API no incluida en la version de Node que estemos usando en el proyecto
     setupFiles: [
         './jest.setup.js'
-    ]
+    ],
+    collectCoverage: true,                          // permite recopilar cobertura
+    collectCoverageFrom: ['src/**/*.{js,jsx}'],     // especifica los archivos para recopilar cobertura de esto será de archivos archivos en todos .jsy jsxde la carpeta src
+    coverageDirectory: 'coverage',                  // especifica la carpeta jest colocará los archivos de cobertura
+    testEnvironment: 'jsdom',                       // El entorno de prueba que se usará para probar tenga en cuenta que lo estamos configurando jsdomy esto provendrá de @testing-library/jest-domlos jest-environment-jsdompaquetes que instalamos anteriormente.
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],// esto le dirá a jest para cada prueba que escribimos, cargará la configuración de jest.setup.js, es decir, use React testing libraly, su jest.config.js
 }
--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
 // Código que se va a ejecutar justo inician las pruebas
 import 'whatwg-fetch';
+import '@testing-library/jest-dom';
 
 
